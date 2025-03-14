9bce9ed782adb75a010997c590154f01ea879841
Edita Zunic <edita.zunic@gmail.com>
2025-03-14 11:39:18 +0100

    fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,11 +1,22 @@
-export function fibonacci (tall) {
+/*export function fibonacci (tall) {
 if (tall===1)
     return 1
 if (tall===2)
     return 1
 return fibonacci(tall-2)+fibonacci(tall-1)
+} */
+
+
+
+
+export function fibonacci (tall) {
+    let i = 1
+    let j = 1
+    let k = 1
+    for(let a = 3; a <= tall; a++) {
+        i = j 
+        j = k 
+        k = i+j
+    }
+    return k
 }
-
-
-
-
